1c90fd2e676d8d38019809b1c3c37c7db32f04f6
Abhik <abhikgupta01@gmail.com>
2024-06-22 04:16:10 +0530

    LAZAREV

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ function navbarAnimation() {
         tl.to("#nav-bottom", {
             height: "11vmax",
         })
-            .to("#nav-center > .nav-elem > h5", {
-                opacity: 1,
-                duration0: 0.2,
-                delay: -1
-            })
-            .to("#nav-center > .nav-elem > h5 > span", {
-                y: 0,
-                duration: 0.2,
-                stagger: 0.04
-            });
+        .to("#nav-center > .nav-elem > h5", {
+            opacity: 1,
+            duration0: 0.2,
+            delay: -1
+        })
+        .to("#nav-center > .nav-elem > h5 > span", {
+            y: 0,
+            duration: 0.2,
+            stagger: 0.04
+        });
     });
     navbar.addEventListener("mouseleave", () => {
         let tl = gsap.timeline();
@@ -24,13 +24,14 @@ function navbarAnimation() {
             y: 30,
             stagger: 0.04
         })
-            .to("#nav-center > .nav-elem > h5", {
-                opacity: 0,
-            })
-            .to("#nav-bottom", {
-                height: "0vmax",
-                delay: -0.8
-            })
+        .to("#nav-center > .nav-elem > h5", {
+            opacity: 0,
+        })
+        .to("#nav-bottom", {
+            height: "0vmax",
+            delay: -0.8
+        });
     });
 }
-navbarAnimation();
+// navbarAnimation();
+
